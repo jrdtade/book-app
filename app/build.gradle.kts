76dd0aae9a88b0787a2cae37f2e85fbc59cd3302
apple <dbossoguntade@gmail.com>
2026-06-21 21:54:37 +0100

    local commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,9 +71,9 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-runtime:2.8.4")
+    implementation("androidx.room:room-ktx:2.8.4")
+    kapt("androidx.room:room-compiler:2.8.4")
 
     implementation("androidx.datastore:datastore-preferences:1.1.1")
 
